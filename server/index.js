0a6318ab7987e17db2cfed32e0233c4920c2b8cb
Pulkit Sujaan <pulkitsujaan2708@gmail.com>
2026-04-11 23:22:57 +0530

    Setup basic structure

--- server/index.js
+++ server/index.js
@@ -4,7 +4,9 @@ const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
+const io = new Server(httpServer, { 
+    cors: { origin: 'http://localhost:3000', methods: ['GET', 'POST'] }
+ });
 
 io.on("connection", (socket) => {
   console.log("Connection Successful");
