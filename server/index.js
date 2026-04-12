85bc52511c669b806357a2161036b3a94233803a
Pulkit Sujaan <pulkitsujaan2708@gmail.com>
2026-04-12 06:11:13 +0530

    Configured env

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const http = require('http');         // built into Node, no install needed
+const cors = require('cors')
 const { Server } = require('socket.io');
 const dbConnect = require('./config/db');
-const initSocket = require('./socket/socket.handler');
+const initSocket = require('./socket/socketHandler');
 const app = express();
 const server = http.createServer(app);  // wrap express in http server
+const dotenv = require('dotenv');
 
-
+dotenv.config();
 const io = new Server(server, {
     cors: {
         origin: "http://localhost:3000",  // your React port
@@ -16,10 +18,11 @@ const io = new Server(server, {
 
 
 app.use(express.json());
+app.use(express.urlencoded());
 app.use(cors());
 
-app.use('/api/auth', require('./routes/auth.routes'));
-app.use('/api/messages', require('./routes/message.routes'));
+app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/messages', require('./routes/messageRoutes'));
 
 const startServer = async () => {
     await dbConnect();
--- server/models/User.js
+++ server/models/User.js
@@ -16,7 +16,7 @@ const UserSchema = new mongoose.Schema({
     required: true,
   },
   messages:[{
-    type: Schema.Types.ObjectId,
+    type: mongoose.Schema.Types.ObjectId,
     ref: "Message"
   }]
 
